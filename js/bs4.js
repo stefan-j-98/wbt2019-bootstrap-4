1eb5b994b4077cf149fc76eb22b8d70c653342ea
Stefan Jovanovic <stefan.jovanovic@rbt.rs>
2019-02-23 13:15:36 +0100

    - add transparent navbar color on bs4 page

--- js/bs4.js
+++ js/bs4.js
@@ -1,3 +1,13 @@
 window.onload=function(){
-    $(".navbar").css("background-color", "#212529");
+    $(".navbar").css("background-color", "#363334");
+    var $win = $(window);
+    $win.scroll(function () {
+        if ($win.scrollTop() == 0) {
+            $(".navbar").css("background-color", "#363334");
+        }
+    });
+    window.onscroll = function () {
+        ScrollEffect()
+        $(".navbar").css("background-color", "#212529");
+    };
 };
